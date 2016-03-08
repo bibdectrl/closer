1ee10e810e1b31add8cfce54ba744a3832ec3e55
bibdectrl <patrick.mooney@ryerson.ca>
2016-03-08 18:41:13 -0500

    added cruddy graphics

--- js/game.js
+++ js/game.js
@@ -1,54 +1,64 @@
 var loadState = {
   preload: function(){
    // load platform graphics
-   // game.load.image("platform-half", "res/platform-half.png");
-   // game.load.image("platform-full", "res/platform-full.png");
+   game.load.spritesheet("platforms", "res/platforms.png", 32, 32);
+   game.load.spritesheet("doors", "res/doors.png", 64, 32);
 
    // load monsters spritesheet
-   // game.load.spritesheet("monster", "res/monster.png", 16, 16);
+   game.load.image("monster", "res/monster.png");
 
    // load player spritesheet
-   // game.load.spritesheet("player", "res/player.png", 16, 16);
+   game.load.image("player", "res/player.png");
 
    // load door spritesheet
    // game.load.spritesheet("door", "res/door.png", 16, 16);
 
    // load tilemap
-   // game.load.tilemap("world", "res/world.json", null, Phaser.Tilemap.TILED_JSON);
+   game.load.tilemap("world", "res/world.json", null, Phaser.Tilemap.TILED_JSON);
 
 
   },
   create: function(){
-    game.state.load("game");
+    game.state.start("game");
   },
 };
 
 var gameState = {
   create: function(){
+   game.stage.backgroundColor = '#787878';
    // start physics
    game.physics.startSystem(Phaser.Physics.ARCADE);
    game.physics.arcade.gravity.y = 200;
    // create map
    this.map = game.add.tilemap("world");
    // add tile images
-   this.map.addTilesetImage("", ""); 
+   this.map.addTilesetImage("platforms", "platforms"); 
+   this.map.addTilesetImage("doors", "doors");
 
    // create map layers
 
-   this.layer = this.map.addLayer("platformLayer");
-   this.layer.setCollision();
+   this.layer = this.map.createLayer("platform-layer");
+   this.map.setCollisionBetween(1, 4);
+   //this.layer.setCollision();
    this.layer.resizeWorld();
 
    // create player sprite
      // find spawn point in map
-     // var x, y =  
+     // var x, y = 
+   var x = 400;
+   var y = 400; 
    this.player = game.add.sprite(x, y, "player");
    game.physics.arcade.enable(this.player);
 
    // create enemies group
    this.enemies = game.add.group();
-   this.enemies.physics.enableBody = true;
+   this.enemies.enableBody = true;
    this.enemies.createMultiple(100, "monster");
+   for (var i = 0; i < 100; i++){
+     var monster = this.enemies.getFirstDead();
+     monster.reset(game.rnd.between(0, 600), game.rnd.between(0, 2000));
+   }
+   game.camera.follow(this.player);
 
   },
 
@@ -63,7 +73,18 @@ var gameState = {
    // update monster logic
 
    // handle input
-
+   if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
+      this.player.body.velocity.x = -200;
+   }
+   else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+      this.player.body.velocity.x = 200;
+   } 
+   else {
+      this.player.body.velocity.x = 0;
+   }
+   if (this.player.body.velocity.y == 0 && game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)){
+      this.player.body.velocity.y = -400;
+   }
   },
 
 
